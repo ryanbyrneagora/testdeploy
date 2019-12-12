e5ed7ccc631a6fe15a83ed80c2b81ca713212b5c
Ryan Byrne <ryan.byrne@agora.io>
2019-12-11 18:52:26 -0600

    updated dependancies 5

--- OpenVideoCall-Web/server.js
+++ OpenVideoCall-Web/server.js
@@ -1,14 +1,8 @@
-const express = require('express');
-const path = require('path');
-const port = process.env.PORT || 8080;
-const app = express();
-
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-
-// send the user to index html page inspite of the url
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './src/pages/index/index.html'));
-});
-
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 5000;
 app.listen(port);
+console.log('server started '+ port);
